6a654893c5940368940397fd7fde8a391f38509f
Mila <m9234434@gmail.com>
2024-03-18 01:03:41 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,29 +1,12 @@
-}
-        for (int item : data) {
-
+        }
     }
-            System.out.println("Random Number: " + item);
-        }
-
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
     public static void main(String[] args) {
-public class RandomDataGenerator {
-        for (int i = 0; i < 10; i++) {
         int[] data = new int[10];
-main();
-    return data;
-    const data = generateRandomData();
 }
-function generateRandomData() {
+public class RandomDataGenerator {
+        }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
 
-}
-    data.forEach(item => {
-    });
-    for (let i = 0; i < 10; i++) {
-        console.log("Random Number: " + item);
-    const data = [];
-    }
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
+        for (int i = 0; i < 10; i++) {
